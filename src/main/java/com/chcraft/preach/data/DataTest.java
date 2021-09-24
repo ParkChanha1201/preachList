51a034a7e0e69c213823dd5151d6492fccefecd9
박찬하 <chanhacraft@gmail.com>
2021-09-25 02:44:30 +0900

    remove unused import

--- src/main/java/com/chcraft/preach/data/DataTest.java
+++ src/main/java/com/chcraft/preach/data/DataTest.java
@@ -2,14 +2,12 @@ package com.chcraft.preach.data;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Random;
 
